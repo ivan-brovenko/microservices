7b12b2f0225f2db6e4f35170e7f097e8d3d6d586
Ivan Brovenko <ivan_brovenko@epam.com>
2019-02-05 16:46:47 +0200

    improved transfer method

--- db-transfer-service/src/main/java/com/istore/dbtransferservice/resources/TransferResource.java
+++ db-transfer-service/src/main/java/com/istore/dbtransferservice/resources/TransferResource.java
@@ -1,39 +1,48 @@
 package com.istore.dbtransferservice.resources;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.List;
-
 @RestController
 @RequestMapping("/transfer")
 public class TransferResource {
 
-    @PostMapping("/roles")
-    public List<String> transferUsersFromMySQLToMongo() {
+    @PostMapping("/mongo")
+    public void transferMySQLToMongo() {
+        transfer("http://localhost:8081/istore/roles/all", "http://localhost:8083/istore/roles/add");
+        transfer("http://localhost:8081/istore/usesr/all", "http://localhost:8083/istore/users/add");
+        transfer("http://localhost:8081/istore/orders/all", "http://localhost:8083/istore/orders/add");
+        transfer("http://localhost:8081/istore/categories/all", "http://localhost:8083/istore/categories/add");
+        transfer("http://localhost:8081/istore/products/all", "http://localhost:8083/istore/products/add");
+    }
+
+    @PostMapping("/mysql")
+    public void transferMongoToMysql() {
+        transfer("http://localhost:8083/istore/roles/all", "http://localhost:8081/istore/roles/add");
+        transfer("http://localhost:8083/istore/usesr/all", "http://localhost:8081/istore/users/add");
+        transfer("http://localhost:8083/istore/orders/all", "http://localhost:8081/istore/orders/add");
+        transfer("http://localhost:8083/istore/categories/all", "http://localhost:8081/istore/categories/add");
+        transfer("http://localhost:8083/istore/products/all", "http://localhost:8081/istore/products/add");
+    }
+
+    private void transfer(String from, String to) {
         RestTemplate restTemplate = new RestTemplate();
 
-        String response = restTemplate.getForObject("http://localhost:8081/istore/roles/all", String.class);
+        String response = restTemplate.getForObject(from, String.class);
         JSONArray jsonArray = new JSONArray(response);
         jsonArray.forEach(role -> {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            restTemplate.exchange("http://localhost:8083/istore/roles/add", HttpMethod.POST, new HttpEntity<>(role.toString(), headers), new ParameterizedTypeReference<String>() {
+            restTemplate.exchange(to, HttpMethod.POST, new HttpEntity<>(role.toString(), headers), new ParameterizedTypeReference<String>() {
             });
         });
-        return Collections.emptyList();
     }
 }
