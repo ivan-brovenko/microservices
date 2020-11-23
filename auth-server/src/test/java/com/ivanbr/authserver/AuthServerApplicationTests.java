c1eeb65ffbefcc82af891ed645b52848cb8e8de8
ivan-br <ivanbbbr@gmail.com>
2020-11-23 18:26:47 +0200

    MICROSERVICER - Initial commit

---
+++ auth-server/src/test/java/com/ivanbr/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanbr.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-server/src/test/java/com/ivanbr/productserver/ProducTserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanbr.productserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducTserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
